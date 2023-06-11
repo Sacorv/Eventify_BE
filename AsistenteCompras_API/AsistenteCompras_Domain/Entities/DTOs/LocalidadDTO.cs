80b2e3e8a276457a402866c3e4ca611c6dc74ecd
godiee <godiework@hotmail.com>
2023-06-11 02:38:32 -0300

    refactor:archivos
    Se eliminaron archivos innecesarios para la estructura actual del proyecto

--- AsistenteCompras_API/AsistenteCompras_Domain/Entities/DTOs/LocalidadDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AsistenteCompras_Entities.DTOs;
-
-public class LocalidadDTO
-{
-    public int Id { get; set; }
-
-    public String Nombre { get; set; } = String.Empty;
-}
--- AsistenteCompras_API/AsistenteCompras_Entities/DTOs/ProductosABuscarDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AsistenteCompras_Entities.DTOs;
-
-public class ProductosABuscarDTO
-{
-    public List<int> IdComidas { get; set; } = new List<int>();
-
-    public List<int> IdBebidas { get; set; } = new List<int>();
-}
