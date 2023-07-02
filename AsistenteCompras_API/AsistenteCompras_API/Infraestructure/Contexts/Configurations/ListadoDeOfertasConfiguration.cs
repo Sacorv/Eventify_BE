11a3d9e5e31e2b69a8c2d42a017aa62f7aa817aa
ScarletCortes <scortesvaldes@alumno.unlam.edu.ar>
2023-07-02 16:12:58 -0300

    upgrade atributos listadoDeOfertas

--- AsistenteCompras_API/AsistenteCompras_API/Infraestructure/Contexts/Configurations/ListadoDeOfertasConfiguration.cs
+++ AsistenteCompras_API/AsistenteCompras_API/Infraestructure/Contexts/Configurations/ListadoDeOfertasConfiguration.cs
@@ -12,8 +12,8 @@ namespace AsistenteCompras_API.Infraestructure.Contexts.Configurations
 
             builder.Property(e => e.Total).HasColumnType("decimal(18, 2)");
             builder.Property(e => e.DistanciaARecorrer).HasColumnType("decimal(18, 2)");
-            builder.Property(e => e.UrlRecorrido).HasMaxLength(2000);
-            builder.Property(e => e.MensajeOfertas).HasMaxLength(2000);
+            builder.Property(e => e.UrlRecorrido).HasColumnType("nvarchar(max)");
+            builder.Property(e => e.MensajeOfertas).HasColumnType("nvarchar(max)");
 
             builder.HasOne(d => d.IdUsuarioNavigation).WithMany(p => p.ListadoDeOfertas)
                 .HasForeignKey(d => d.IdUsuario)
